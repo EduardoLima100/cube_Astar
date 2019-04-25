442fbfe69435134befb1f5e5131a6c5399c86cd6
EduardoLima100 <eduardolima100@gmail.com>
2019-04-25 11:07:30 -0300

    Implementando análise de tempo

--- cube_Astar.py
+++ cube_Astar.py
@@ -1,6 +1,7 @@
 import random
+import time
 
-TAM = 4
+TAM = 10
 BLOCK = 0.4
 OP = [[1,0,0],      # >
       [-1,0,0],     # <
@@ -9,27 +10,31 @@ OP = [[1,0,0],      # >
       [0,0,1],      # x
       [0,0,-1]]     # .
 
-NODES = []
-
 def rand_xyz():
     return [random.randint(0,TAM-1),
             random.randint(0,TAM-1),
             random.randint(0,TAM-1)]
 
-BLOCKED = []
-while len(BLOCKED) < int(TAM**3*BLOCK):        
-    xyz = rand_xyz()
-    if xyz not in BLOCKED:
-        BLOCKED.append(xyz)
-print("BLOCKED = ",BLOCKED)
 
-GAME = []
-while len(GAME) < 2:        
-    xyz = rand_xyz()
-    if xyz not in BLOCKED and xyz not in GAME:
-        GAME.append(xyz)
-print("\nInício: ", GAME[0], "\nObjetivo: ", GAME[1])
+def set_BLOCKED():    
+    global BLOCKED
+    BLOCKED = []
+    while len(BLOCKED) < int(TAM**3*BLOCK):        
+        xyz = rand_xyz()
+        if xyz not in BLOCKED:
+            BLOCKED.append(xyz)
+    #print("\n\nBLOCKED = ",BLOCKED)
+
+def set_GAME():    
+    global GAME
+    GAME = []
+    while len(GAME) < 2:        
+        xyz = rand_xyz()
+        if xyz not in BLOCKED and xyz not in GAME:
+            GAME.append(xyz)
     
+    #print(time.strftime("\n[%H:%M:%S]"))
+    #print("Início: ", GAME[0], "\nObjetivo: ", GAME[1])
 
 class Node:
     def __init__(self,x,y,z):
@@ -58,7 +63,7 @@ class Node:
             return False
     
     def next_Node(self):
-        men_custo = TAM**3
+        men_custo = TAM**3 - (TAM**3)*BLOCK
         for op in OP:
             new = Node(self.xyz[0]+op[0],self.xyz[1]+op[1],self.xyz[2]+op[2])
             
@@ -75,27 +80,42 @@ class Node:
     def calc_Dist(self):
         s = 0
         for i in range(3):
-            s = (GAME[1][i]-self.xyz[i])**2
+            s = s + float((GAME[1][i]-self.xyz[i])**2)
         #print("Dist: ",s)
-        return s**(1/2)
+        return float(s**(1/2))
     
     def calc_Custo(self):
         return self.way + self.dist
     
     def __str__(self):
         return str(self.xyz)
-        
+       
+
 def main():
+    global NODES
+    NODES = []
+    
+    set_BLOCKED()
+    set_GAME()
+    
     start_Node = Node(GAME[0][0],GAME[0][1],GAME[0][2])
+    #print("Distância em linha reta:", start_Node.calc_Dist())
+    
     NODES.append(start_Node)
 
     loop = True
     while loop:
+        
+        if((maior_tempo > 0) and (time.time() - t0 > maior_tempo*10)):
+            return 1
         if len(NODES) > 0:
             #print(NODES[0])
             if NODES[0].is_Objetivo:
-                print("Objetivo encontrado!")
-                print(NODES[0])
+                #print(time.strftime("\n[%H:%M:%S]"))
+                #print("Objetivo encontrado!")
+                print('.', end='')
+                #print(NODES[0])
+                #print("Distância percorrida:", NODES[0].way)
                 loop = False
             else:
                 NODES[0].next_Node()
@@ -104,9 +124,27 @@ def main():
                 NODES.append(n)
             
             NODES.remove(NODES[0])
+            
         else:
-            print("Todas as possibilidades foram testadas\nSolução não existe")
+            #print(time.strftime("\n[%H:%M:%S]"))
+            #print("Todas as possibilidades foram testadas\nSolução não existe")
+            print('-', end='')
             loop = False
-
+        
+                   
 if __name__ == "__main__":
-    main()
+    global t0;
+    print(time.strftime("\n[%H:%M:%S]"))
+    global maior_tempo
+    maior_tempo = 0
+    while True:
+        
+        t0 = time.time()
+        if main() == 1:
+            print("/", end='')
+        else:
+            tf = time.time()
+            #print("Tempo gasto:", tf-t0)
+            if tf-t0 > maior_tempo:
+                maior_tempo = tf-t0
+                print("\nMaior tempo:",maior_tempo)
