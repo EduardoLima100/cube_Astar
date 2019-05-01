d6c216a34944ee79d5c268ea1fe1984d6760277f
EduardoLima100 <eduardolima100@gmail.com>
2019-05-01 09:27:14 -0300

    Update show_times_data.py
    Agora mostra apenas resultados que obtiveram mais do que 50 amostras

--- show_times_data.py
+++ show_times_data.py
@@ -1,29 +1,34 @@
 import pickle
 import matplotlib.pyplot as plt
 
-global DATA
-
-def print_DATA():
+def print_DATA(DATA):
     print("Distância:","\tTempo médio:","\t\t\tOcorrências:")
-    for i in range(1,len(DATA)):
-        print(i,"\t|\t","%.20f"%DATA[i][0],"\t|\t",DATA[i][1])
+    for i in range(len(DATA)):
+        print(i+1,"\t|\t","%.20f"%DATA[i][0],"\t|\t",DATA[i][1])
 
 try:
     with open('times_data', 'rb') as fp:
         DATA = pickle.load(fp)
         fp.close()
-        print_DATA()
 except IOError:
     print("Não há dados")
     
-X = range(len(DATA))
-Y = []
-for y in DATA:
-    Y.append(y[0])
-
-plt.rcParams['figure.figsize'] = (7,7)
-plt.plot(X,Y)
-plt.grid()
-plt.show()
+if(len(DATA)>0):
+    DT = []
+    for i in range(len(DATA)):
+        if DATA[i][1] > 50:
+            DT.append(DATA[i])
+    
+    X = range(1,len(DT)+1)
+    Y = []
+    for y in DT:
+        Y.append(y[0])
+    
+    print_DATA(DT)
+    
+    plt.rcParams['figure.figsize'] = (7,7)
+    plt.plot(X,Y)
+    plt.grid()
+    plt.show()
 
         
