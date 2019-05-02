f051f24167be59a0b185d7d7841cac80afd7ccba
EduardoLima100 <eduardolima100@gmail.com>
2019-05-02 01:19:23 -0300

    show_times_data.py mostrando apenas amostras com mais de 50 ocorrências

--- show_times_data.py
+++ show_times_data.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 def print_DATA(DATA):
     print("Distância:","\tTempo médio:","\t\t\tOcorrências:")
     for i in range(len(DATA)):
-        print(i+1,"\t|\t","%.20f"%DATA[i][0],"\t|\t",DATA[i][1])
-
+        print(DATA[i][0],"\t|\t","%.20f"%DATA[i][1],"\t|\t",DATA[i][2])
 try:
     with open('times_data', 'rb') as fp:
         DATA = pickle.load(fp)
@@ -16,15 +15,25 @@ except IOError:
 if(len(DATA)>0):
     DT = []
     for i in range(len(DATA)):
-        if DATA[i][1] > 50:
-            DT.append(DATA[i])
+        DT.append([i]+DATA[i])
     
-    X = range(1,len(DT)+1)
-    Y = []
-    for y in DT:
-        Y.append(y[0])
+    i = 0
+    while i < len(DT):
+        if DT[i][2] < 50:
+            DT.remove(DT[i])
+        else:
+            i=i+1
     
     print_DATA(DT)
+    
+    X = []
+    for x in DT:
+        X.append(x[0])
+    Y = []
+    for y in DT:
+        Y.append(y[1])
+    
+    
     
     plt.rcParams['figure.figsize'] = (7,7)
     plt.plot(X,Y)
